e0014b71459e0888b7f1193c0fa1563fa6677bf1
Guy <guymoustapha@gmail.com>
2023-05-15 22:17:56 +0200

    update invoices controller to add and update new invoices

--- controllers/invoice_controller.js
+++ controllers/invoice_controller.js
@@ -1,6 +1,7 @@
-const Invoices_items = require('../db/models/invoice_items');
-const Invoices = require('../db/models/invoices');
-
+const { transaction } = require("objection");
+const Invoices_items = require("../db/models/invoice_items");
+const Invoices = require("../db/models/invoices");
+const Items = require("../db/models/items");
 
 //@desc  Get all the invoices
 //@routes get /api/clients
@@ -18,138 +19,187 @@ const Invoices = require('../db/models/invoices');
 //     }
 // };
 
+const getInvoices = async (ctx) => {
+  try {
+    const foundInvoices_items = await Invoices.query();
+    // const result = await knex('invoices')
+    //   return result;
 
-const getInvoices = async ctx => {
-    try {
-        const foundInvoices_items = await Invoices.query()
-            // const result = await knex('invoices')
-            .select('invoices.id as invoice_id',
-                'invoices.customer_name',
-                'items.item_name',
-                'invoice_items.unit_price')
-            .sum('invoice_items.total_price as total_amount')
-            // .join('invoice_items', 'invoices.id', '=', 'invoice_items.invoice_id')
-            .join('invoice_items', 'invoices.id', '=', 'invoice_items.invoice_id')
-            .join('items', 'invoice_items.item_id', '=', 'items.id')
-
-            // .groupBy('invoices.id');
-            .groupBy(
-                'invoices.id',
-                'invoices.customer_name',
-                'items.item_name',
-                'invoice_items.unit_price'
-            );
-        //   return result;
-
-        ctx.body = {
-            message: 'Created!',
-            data: foundInvoices_items
-
-        };
-        ctx.status = 200;
-    } catch (err) {
-        ctx.body = err;
-        ctx.status = 500;
-    }
+    ctx.body = {
+      message: "Created!",
+      data: foundInvoices_items,
+    };
+    ctx.status = 200;
+  } catch (err) {
+    ctx.body = err;
+    ctx.status = 500;
+  }
 };
 
-let id = null
+let id = null;
 
-const getInvoiceDetails = async ctx => {
-    try {
-        const invoice_id = ctx.params.id
-        console.log('invoice_id', invoice_id)
-        const result = await Invoices.query()
-            .select('invoices.*', 'invoice_items.*', 'items.item_name')
-            .join('invoice_items', 'invoices.id', '=', 'invoice_items.invoice_id')
-            .join('items', 'invoice_items.item_id', '=', 'items.id')
-            .where('invoices.id', invoice_id);
-        // return result;
-        ctx.body = {
-            message: 'invoice details!',
-            data: result
+const getInvoiceDetails = async (ctx) => {
+  try {
+    const invoice_id = ctx.params.id;
+    console.log("invoice_id", invoice_id);
+    const result = await Invoices.query()
+      .select("invoices.*", "invoice_items.*", "items.item_name")
+      .join("invoice_items", "invoices.id", "=", "invoice_items.invoice_id")
+      .join("items", "invoice_items.item_id", "=", "items.id")
+      .where("invoices.id", invoice_id);
+    // return result;
+    ctx.body = {
+      message: "invoice details!",
+      data: result,
+    };
+    ctx.status = 200;
+  } catch (err) {
+    ctx.body = err;
+    ctx.status = 500;
+  }
+};
 
-        };
-        ctx.status = 200;
-    } catch (err) {
-        ctx.body = err;
-        ctx.status = 500;
-
-    }
-}
-
-//@desc  Post client 
+//@desc  Post client
 //@routes post /api/Invoices
 const postInvoices = async (ctx) => {
-    try {
-        const uniqueId = (length = 8) => {
-            return parseInt(Math.ceil(Math.random() * Date.now()).toPrecision(length).toString().replace(".", ""))
+  const { customer_name, number, items } = ctx.request.body;
+  const uniqueId = (length = 8) => {
+    return parseInt(
+      Math.ceil(Math.random() * Date.now())
+        .toPrecision(length)
+        .toString()
+        .replace(".", "")
+    );
+  };
+  const def = uniqueId();
+
+  try {
+    await transaction(Invoices.knex(), async (trx) => {
+      const invoice = await Invoices.query(trx).insert({
+        customer_name,
+        number: def,
+        total_amount: 0,
+      });
+
+      for (const item of items) {
+        const { item_id, quantity } = item;
+        // Get the item
+        const dbItem = await Items.query(trx).findById(item_id);
+
+        if (!dbItem) {
+          throw new Error(`Item with id ${item_id} not found`);
         }
-        const def = uniqueId()
-        const { number, customer_name, total_amount } = ctx.request.body;
-        await Invoices.query().insert({
-            number: def,
-            customer_name: customer_name,
-            total_amount: total_amount
+        // Calculate the total price for this item
+        const unit_price = dbItem.sale_price;
+        const total_price = unit_price * quantity;
+
+        // Create the invoice item
+        await Invoices_items.query(trx).insert({
+          invoice_id: invoice.id,
+          item_id,
+          quantity,
+          unit_price,
+          total_price,
         });
-        ctx.body = {
-            message: 'invoice Created!',
-            data: {
-                number,
-                customer_name,
-                total_amount
-            }
-        }
-        ctx.status = 201;
-    } catch (err) {
-        ctx.status = 500;
-        throw (err)
-    }
+
+        invoice.total_amount += total_price;
+        await invoice.$query(trx).patch({ total_amount: invoice.total_amount });
+
+        // await Items.query(trx)
+        //   .findById(item_id)
+        //   .patch({ quantity: dbItem.quantity - quantity });
+      }
+    });
+
+    ctx.status = 201;
+    ctx.body = { message: "Invoice created successfully" };
+  } catch (error) {
+    console.error(error);
+    console.error(ctx.request.body);
+    ctx.status = 500;
+    ctx.body = { message: "Internal server error" };
+  }
 };
 
 const updateInvoice = async (ctx) => {
-    try {
-        const invoice_id = ctx.params.id
-        console.log('invoice_id', invoice_id);
-        const updatedData = ctx.request.body;
-        const result = await Invoices.query()
-            .where('id', invoice_id)
-            .update(updatedData);
-        // return result;
-        ctx.body = {
-            message: 'Invoice updated successfully',
-            data: result
+  try {
+    // Start a transaction
+    await transaction(Invoices.knex(), async (trx) => {
+      const id = ctx.params.id;
+      const { customer_name, items } = ctx.request.body;
+      // Get the invoice
+      const invoice = await Invoices.query(trx).findById(id);
 
-        };
-        ctx.status = 200;
-    } catch (err) {
-        ctx.body = { message: 'Error updating invoice' };
-        ctx.status = 500;
+      // Delete all existing invoice_items for this invoice
+      await Invoices_items.query(trx).delete().where("invoice_id", id);
 
-    }
+      // Loop through the items and create invoice_items
+      for (const item of items) {
+        const { item_id, quantity } = item;
+
+        // Get the item
+        const dbItem = await Items.query(trx).findById(item_id);
+
+        // Calculate the total price
+        const total_price = dbItem.sale_price * quantity;
+
+        // Create the invoice_item
+        await Invoices_items.query(trx).insert({
+          invoice_id: invoice.id,
+          item_id,
+          quantity,
+          unit_price: dbItem.sale_price,
+          total_price,
+        });
+
+        // Update the total amount of the invoice
+        invoice.total_amount += total_price;
+      }
+
+      // Update the invoice with the new details
+      await invoice
+        .$query(trx)
+        .patch({ customer_name, total_amount: invoice.total_amount });
+    });
+    ctx.status = 200;
+    ctx.body = {
+      message: "Invoice updated successfully",
+      //   data: result,
+    };
+  } catch (err) {
+    console.log("====>err", err);
+    ctx.body = { message: "Error updating invoice, try again" };
+    ctx.status = 500;
+  }
 };
 
 const deleteInvoice = async (ctx) => {
-    try {
-        const invoice_id = ctx.params.id
-        console.log('invoice_id', invoice_id);
-        // const updatedData = ctx.request.body;
-        const result = await Invoices.query()
-            .where('id', invoice_id)
-            .del();
-        ctx.body = {
-            message: 'Invoice deleted successfully',
-            data: result
+  try {
+    await transaction(Invoices.knex(), async (trx) => {
+      const id = ctx.params.id;
+      console.log("invoice_id", id);
+      // Delete all invoice_items for this invoice
+      await Invoices_items.query(trx).delete().where("invoice_id", id);
+      // Delete the invoice
+      await Invoices.query(trx).deleteById(id);
+    });
 
-        };
-        ctx.status = 200;
-    } catch (err) {
-        ctx.body = { message: 'Error deleteng invoice' };
-        ctx.status = 500;
-
-    }
+    ctx.status = 200;
+    ctx.body = {
+      message: "Invoice deleted successfully",
+      // data: result,
+    };
+  } catch (err) {
+    console.log("errrrrr", err);
+    ctx.body = { message: "Error deleteng invoice" };
+    ctx.status = 500;
+  }
 };
 
-
-
-module.exports = { getInvoices, postInvoices, getInvoiceDetails, updateInvoice, deleteInvoice };
+module.exports = {
+  getInvoices,
+  postInvoices,
+  getInvoiceDetails,
+  updateInvoice,
+  deleteInvoice,
+};
